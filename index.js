d73cd98a3461db0c6adef432b05ab2e80d48c0e5
mogdanova <56578892+mogdanova@users.noreply.github.com>
2020-03-12 08:53:29 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,96 +1,148 @@
-var result = document.getElementById('result');  
-var input = document.querySelector("input");
-var num1 = document.getElementById('num1');
-var num2 = document.getElementById("num2");
-var a = Number(num1.value);
-var b = Number(num2.value);
-function sum(x, y){
-  result.value = x + y;
-}
+var btn = document.querySelector('button');
+var inputVal = document.getElementById("input").value; 
+var checkVal = document.getElementById("check").value;
+var outputVal = document.getElementById("output");
 
-function sub(x, y){
-  result.value = x-y;
-}
-
-function mul(x, y){
-  result.value = x*y;
-}
-
-function percent(x, y){
-  if(x > 0){
-    result.value = (y*100)/x;
-  } else {
-      result.value = "underfined";
+function reverse(s,t) {
+  t = '';
+  for (var i = s.length - 1; i >= 0; i--){
+      t = t += s[i];
   }
+  return t;
 }
 
-function div(x, y){
-  result.value = x/y;
-}
-
-function intdiv(x, y){
-  result.value = (x / y);
-  result.value = result.value - (result.value%1);
-}
-
-function abs(x){
-  if(x < 0) {
-    result.value = (x ** 2) ** 0.5;
-  } else { result.value = x }
-}
-
-
-function sqr(x){
-  result.value = x ** 2;
-}
-
-function factorial(x) {
-    if (x > 1){
-    return x ? x * factorial(x - 1) : 1;
-  } else {
-      if(x == 1) {
-        return 1;
-      } else {
-        result.value = "underfined";
+function endWith(s,t,c){
+  t = '';
+  var n = 0;
+  if(c != ''){
+    for(var i = (s.length - c.length); i < s.length; i++){
+      for(var j = 0; j < c.length; j++){
+        if(s[i] == c[j]) n++;
       }
+    }
+    if(n == c.length) {
+      t = true;
+    }
+    else t = false;
   }
+  else alert("Enter some end to check it!");
+  return t;
 }
 
-function fact(x) {
-  var result = document.getElementById('result');  
-  result.value = factorial(x);
+function startWith(s,t,c){
+  t = '';
+  var n = 0;
+  if(c != ''){
+    for(var i = 0; i < c.length; i++){
+      if(s[i] == c[i]) n++;
+    }
+    if(n == c.length) {
+      t = true;
+    }
+    else t = false;
+  }
+  else alert("Enter some start to check it!");
+  return t;
 }
 
-function round(x, y, z){
-  y = x - (x%1);
-  z = y+1;
-  var mid = x - y;
-  if(mid >= 0.5) {
-    result.value = z;
-  } else 
-      if(mid < 0.5) {
-        result.value = y;
-      }
+function isCamelCase(s,t){
+  t = '';  
+  var j = 0, k = 0, n =0;
+  for(var i = 0; i < s.length; i++){
+    if((s[i] == s[i].toUpperCase()) && (s[i] != ' '))
+      j++;
+    else 
+      if(s[i] != ' ')
+       k++;
+      else 
+        n++;
+  }
+  if((n == 0) && (k >= 2) && (j >= 2)){
+    t = true;
+  } else t = false;
+  return t;
 }
 
-function sin(x){
-  result.value = Math.sin(x); 
+function isSnakeCase(s,t){
+  t = '';  
+  var j = 0, k = 0, n =0, l = 0;
+
+  for(var i = 0; i < s.length; i++){
+    if((s[i] == s[i].toUpperCase()) && (s[i] != ' ') && (s[i] != '_'))
+      j++;
+    else 
+      if((s[i] != ' ') && (s[i] != '_'))
+       k++;
+      else 
+        if(s[i] != '_')
+          n++;
+        else 
+          l++;
+  }
+
+  if((n == 0) && (k >= 2) && (j == 0) && (l >= 1)){
+    t = true;
+  } else t = false;
+  return t;
 }
 
-function cos(x){
-  result.value = Math.cos(x); 
+function isNan(s,t){
+  t = '';
+  d = '0123456789';
+  var k = 0;
+  for(var i = 0; i < s.length; i++){
+    if(d.search(s[i]) == -1){
+      k++;
+    }
+  }
+  if(k == s.length){
+    t = true;
+  }
+  else 
+    t = false;
+  return t;
 }
 
-function tg(x){
-  result.value = Math.tan(x); 
+function isPhone(s,t) {
+ t = '';
+  var c1 = new RegExp("37544");
+  var c2 = new RegExp("37529");
+  var c3 = new RegExp("8029");
+  var c4 = new RegExp("8044");
+  var t1 = c1.exec(s);
+  var t2 = c2.exec(s);
+  var t3 = c3.exec(s);
+  var t4 = c4.exec(s);
+  if((t1!=null) || (t2!=null) || (t3!=null) || (t4!=null)) {
+    t = true;
+  }
+  else  {
+    t = false;
+    alert("Enter correct phone number");
+  }
+  console.log(t);
+  console.log(s);
+  return t;
 }
 
-function ctg(x){
-  result.value = Math.atan(x); 
-}
-
-function allow_enter(){
-  input.keyup = function(){
-    this.value = this.value.replace(/[^0-9\.]/g, '');
-  };
+function isEmail(s,t){
+  t = '';
+  var c1 = new RegExp("@mail.com ");
+  var c2 = new RegExp("@gmail.com ");
+  var c3 = new RegExp("@mail.ru ");
+  var c4 = new RegExp("@gmail.ru ");
+  var t1 = c1.exec(s);
+  var t2 = c2.exec(s);
+  var t3 = c3.exec(s);
+  var t4 = c4.exec(s);
+  if((t1!=null) || (t2!=null) || (t3!=null) || (t4!=null)) {
+    t = true;
+  }
+  else  {
+    t = false;
+    alert("Enter correct email");
+  }
+  console.log(t);
+  console.log(s);
+  return t;
 }
